7da4392b1e5cebfe4610aff9dd95e24e4a43ca37
Mehdi-Javadzadeh <75339855+Mehdi-Javadzadeh@users.noreply.github.com>
2022-02-10 14:39:29 +0330

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import Image from "next/image";
 import React, { useState } from "react";
 import { Pressable } from "react-native";
