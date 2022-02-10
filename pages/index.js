03b731c0450607088e83e8560aaad99eae26dc16
Mehdi-Javadzadeh <75339855+Mehdi-Javadzadeh@users.noreply.github.com>
2022-02-10 15:38:10 +0330

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1297,7 +1297,7 @@ const styles = StyleSheet.create({
   },
   hotSuggest: {
     marginTop: 15,
-    justifyContent: "flex-end",
+    justifyContent: "center",
     width: "20%",
     padding: 10,
     // marginLeft: 10,
